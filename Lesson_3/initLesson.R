7a1c85df8e69db19904a87e38c1900e25d897b3e
Reggie <68339917+R-Gilliard-Jr@users.noreply.github.com>
2023-04-13 20:27:46 -0400

    Lesson 4 (#3)
    * remove the use of functions from scripts
    
    * update custom tests and sum_x_y-correct
    
    * Add lesson 4. Swap lessons 3 and 4

--- Lesson_3/initLesson.R
+++ Lesson_3/initLesson.R
@@ -2,13 +2,5 @@
       # lesson is started. Any variables created here will show up in
       # the user's working directory and thus be accessible to them
       # throughout the lesson.
-pkgs <- c("dplyr")
-invisible(lapply(pkgs, library, character.only = T))
 
-.get_course_path <- function(){
-  tryCatch(swirl:::swirl_courses_dir(),
-           error = function(c) {file.path(find.package("swirl"),"Courses")}
-  )
-}
-
-nyc_survey_total <- read.csv(file.path(.get_course_path(), "DegreesNYC_RCourse", "Lesson_3", "total.csv"))
+long_vector <- sample(100, 25)
