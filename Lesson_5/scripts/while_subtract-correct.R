4a608339dfc2b3cedbe7c6906b84e1aaec63abed
Reggie Gilliard <regg19970@gmail.com>
2023-04-19 21:37:28 -0400

    Drafting lesson 5

---
+++ Lesson_5/scripts/while_subtract-correct.R
@@ -0,0 +1,9 @@
+i <- 1
+ws_total <- 100
+# While i is less than or equal to 10
+while (i <= 10) {
+  # Subtract i from ws_total and store that in difference
+  ws_total <- ws_total - i
+  # Increment i
+  i <- i + 1
+}
